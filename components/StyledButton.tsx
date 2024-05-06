e0fb53d929a70599a114552af48aef6ccfb91502
Muzammil-cyber <muzammilloya@hotmail.com>
2024-05-06 20:38:25 +0500

    refactor: Update button variant naming and color in StyledButton.tsx and Colors.ts

--- components/StyledButton.tsx
+++ components/StyledButton.tsx
@@ -6,14 +6,13 @@ import {
   View,
 } from "react-native";
 import { useThemeColor } from "./Themed";
-import Colors from "@/constants/Colors";
 
 type ThemeProps = {
   lightColor?: string;
   darkColor?: string;
 };
 
-type ButtonVariant = keyof typeof Colors.light & keyof typeof Colors.dark;
+type ButtonVariant = "primary" | "secondary" | "success" | "danger" | "warning";
 type ButtonTextSize = "s" | "m" | "l";
 
 export type ButtonProps = ThemeProps &
@@ -39,29 +38,25 @@ export const Button = forwardRef<View | null, ButtonProps>((props, ref) => {
     { light: lightColor, dark: darkColor },
     variant ? variant : "primary"
   );
-  const textColor = useThemeColor(
-    { light: lightColor, dark: darkColor },
-    variant && outlined ? variant : "buttonText"
-  );
-  // const textColor = outlined
-  //   ? useThemeColor(
-  //       {
-  //         light: lightColor,
-  //         dark: darkColor,
-  //       },
+  const textColor = outlined
+    ? useThemeColor(
+        {
+          light: lightColor,
+          dark: darkColor,
+        },
 
-  //       variant ? `outline-${variant}` : "outline-primary"
-  //     )
-  //   : useThemeColor(
-  //       {
-  //         light: lightColor,
-  //         dark: darkColor,
-  //       },
-  //       "buttonText"
-  //     );
+        variant ? `outline-${variant}` : "outline-primary"
+      )
+    : useThemeColor(
+        {
+          light: lightColor,
+          dark: darkColor,
+        },
+        "buttonText"
+      );
 
   const backgroundColor = outlined ? "transparent" : color;
-  const borderColor = outlined ? color : "transparent";
+  const borderColor = outlined ? textColor : "transparent";
   const borderWidth = outlined ? 1 : 0;
 
   const fontSize = textSize === "s" ? 12 : textSize === "l" ? 20 : 16;
--- constants/Colors.ts
+++ constants/Colors.ts
@@ -10,18 +10,18 @@ export default {
     tabIconSelected: tintColorLight,
     // button
     primary: '#007bff',
-    secondary: '#000',
+    secondary: '#6c757d',
     success: '#28a745',
     danger: '#dc3545',
     warning: '#ffc107',
     // button text
     buttonText: '#fff',
     // button outline
-    // 'outline-primary': '#007bff',
-    // 'outline-secondary': '#000',
-    // 'outline-success': '#28a745',
-    // 'outline-danger': '#dc3545',
-    // 'outline-warning': '#ffc107',
+    'outline-primary': '#007bff',
+    'outline-secondary': '#000',
+    'outline-success': '#28a745',
+    'outline-danger': '#dc3545',
+    'outline-warning': '#ffc107',
   },
   dark: {
     text: '#fff',
@@ -31,17 +31,17 @@ export default {
     tabIconSelected: tintColorDark,
     // button
     primary: '#007bff',
-    secondary: '#fff',
+    secondary: '#6c757d',
     success: '#28a745',
     danger: '#dc3545',
     warning: '#ffc107',
     // button text
     buttonText: '#fff',
     // button  outline
-    // 'outline-primary': '#007bff',
-    // 'outline-secondary': '#fff',
-    // 'outline-success': '#28a745',
-    // 'outline-danger': '#dc3545',
-    // 'outline-warning': '#ffc107',
+    'outline-primary': '#007bff',
+    'outline-secondary': '#fff',
+    'outline-success': '#28a745',
+    'outline-danger': '#dc3545',
+    'outline-warning': '#ffc107',
   },
 };
