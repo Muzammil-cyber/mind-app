c1847fc224e0d4b2a20d06778cdfda66fbdc4b3b
Muzammil-cyber <muzammilloya@hotmail.com>
2024-05-17 21:18:41 +0500

    refactor: Add task creation functionality and improve UI

--- app/add-task.tsx
+++ app/add-task.tsx
@@ -1,20 +1,65 @@
 import { StatusBar } from "expo-status-bar";
 import { Platform, StyleSheet } from "react-native";
-
-import EditScreenInfo from "@/components/EditScreenInfo";
-import { Text, View } from "@/components/Themed";
+import { View } from "@/components/Themed";
+import { Heading2, TextInput } from "@/components/StyledText";
+import DateTimePicker from "react-native-ui-datepicker";
+import Colors from "@/constants/Colors";
+import { useColorScheme } from "@/components/useColorScheme";
+import { useState } from "react";
+import { Button } from "@/components/StyledButton";
 
 export default function ModalScreen() {
+  const colorScheme = useColorScheme();
+  const [date, setDate] = useState(new Date());
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+
+  const handleSubmit = () => {
+    console.log("Data", {
+      title,
+      description,
+      date,
+    });
+  };
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Modal</Text>
+      <Heading2>Add your Task here</Heading2>
       <View
         style={styles.separator}
         lightColor="#eee"
         darkColor="rgba(255,255,255,0.1)"
       />
-      <EditScreenInfo path="app/modal.tsx" />
-
+      <TextInput
+        label="Title"
+        placeholder="Task Name"
+        onChangeText={setTitle}
+        value={title}
+      />
+      <TextInput
+        label="Description"
+        placeholder="Task Description"
+        onChangeText={setDescription}
+        value={description}
+      />
+      <View
+        style={{
+          ...styles.dateContainer,
+          borderColor:
+            Colors[colorScheme === "dark" ? "light" : "dark"].background,
+        }}
+      >
+        <DateTimePicker
+          mode="single"
+          displayFullDays
+          date={date}
+          // @ts-ignore
+          onChange={(p) => setDate(p?.date)}
+        />
+      </View>
+      <Button style={styles.submitBtn} onPress={handleSubmit}>
+        Add Task
+      </Button>
       {/* Use a light status bar on iOS to account for the black space above the modal */}
       <StatusBar style={Platform.OS === "ios" ? "light" : "auto"} />
     </View>
@@ -26,14 +71,22 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: "center",
     justifyContent: "center",
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: "bold",
+    paddingHorizontal: 20,
   },
   separator: {
-    marginVertical: 30,
+    marginVertical: 10,
     height: 1,
     width: "80%",
+  },
+  dateContainer: {
+    backgroundColor: Colors.light.background,
+
+    borderWidth: 1,
+    borderRadius: 10,
+    overflow: "hidden",
+  },
+  submitBtn: {
+    marginTop: 20,
+    width: "100%",
   },
 });
